ec6f16f08d05f81c4e28a6c5896b170d561bb44b
Andy Roche <andy@roche.io>
2018-03-07 23:25:24 -0500

    add basic Chord struct

--- main/main.go
+++ main/main.go
@@ -24,9 +24,67 @@ func NewInstrument(stringNames []string) Instrument {
 	}
 }
 
+// GetString returns an instruments string by name (e.g. "A")
+func (i Instrument) GetString(name string) (InstrumentString, error) {
+	for _, s := range i.strings {
+		if s.name == name {
+			return s, nil
+		}
+	}
+	var nullString InstrumentString
+	return nullString, fmt.Errorf("string '%s' not found on instrument '%s'", name, i)
+}
+
+// Chord is a fixed position on the frets of an Instrument
+type Chord struct {
+	instrument Instrument
+	positions  map[InstrumentString]int32
+}
+
+// NewChord returns a Chord from a map of strings to positions
+func NewChord(i Instrument, namesToPositions map[string]int32) Chord {
+	strPositions := make(map[InstrumentString]int32)
+	for name, position := range namesToPositions {
+		str, err := i.GetString(name)
+		if err != nil {
+			panic(err)
+		}
+		strPositions[str] = position
+	}
+
+	c := Chord{
+		instrument: i,
+		positions:  strPositions,
+	}
+
+	return c
+}
+
+func printChordAsTab(c Chord) {
+	for _, s := range c.instrument.strings {
+		var displayValue string
+		fret, ok := c.positions[s]
+		if ok {
+			displayValue = fmt.Sprintf("%d", fret)
+		} else {
+			displayValue = "-"
+		}
+		fmt.Println(displayValue)
+	}
+}
+
 func main() {
 	guitar := NewInstrument([]string{
-		"E", "A", "D", "G", "B", "e",
+		"e", "B", "G", "D", "A", "E",
 	})
+
+	c := NewChord(guitar, map[string]int32{
+		"B": 1,
+		"D": 2,
+		"A": 3,
+	})
+
 	fmt.Println(guitar.strings)
+
+	printChordAsTab(c)
 }

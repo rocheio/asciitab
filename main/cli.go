6a9fe3d956027aca652ad308854bbaeca6efcf14
Andy Roche <andy@roche.io>
2018-03-19 22:29:47 -0400

    allow specifying scale in CLI

--- main/cli.go
+++ main/cli.go
@@ -23,32 +23,45 @@ func main() {
 	// all tabs are for guitar currently
 	guitar := NewGuitar()
 
-	// asciitab random
-	if len(os.Args) == 2 && os.Args[1] == "random" {
-		scale := RandomScale()
+	// parse "--abc=123" flags from command line
+	flags := make(map[string]string)
+	for _, arg := range os.Args {
+		// all accepted flags currently start with "--"
+		if arg[:2] != "--" {
+			continue
+		}
+		// See if flag is form of "--flag" or "--flag=value"
+		equalsIndex := strings.Index(arg, "=")
+		if equalsIndex == -1 {
+			flags[arg[2:]] = ""
+			continue
+		}
+		// Flag is form of "--flag=value", set it and trim quotes
+		value := strings.Trim(arg[equalsIndex+1:], `'"`)
+		flags[arg[2:equalsIndex]] = value
+	}
+
+	// asciitab random --key=A#
+	if len(os.Args) >= 2 && os.Args[1] == "random" {
+		// key provided or random key?
+		key, ok := flags["key"]
+		if !ok {
+			key = RandomNote()
+		}
+		// scale provided or random scale?
+		scaleName, ok := flags["scale"]
+		if !ok {
+			scaleName = RandomScaleName()
+		}
+		scale, err := NewScale(scaleName, key)
+		if err != nil {
+			panic(err)
+		}
 		tab := RandomTab(guitar, scale)
 
 		fmt.Printf("Random tab in %s\n", scale)
 		tab.PrintAll()
 		return
-	}
-
-	// asciitab random --key=A#
-	if len(os.Args) == 3 && os.Args[1] == "random" {
-		if os.Args[2][:6] == "--key=" {
-			key := os.Args[2][6:]
-			key = strings.Trim(key, `'"`)
-			scaleName := RandomScaleName()
-			scale, err := NewScale(scaleName, key)
-			if err != nil {
-				panic(err)
-			}
-			tab := RandomTab(guitar, scale)
-
-			fmt.Printf("Random tab in %s\n", scale)
-			tab.PrintAll()
-			return
-		}
 	}
 
 	// default
